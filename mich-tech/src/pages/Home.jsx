5f614ace7ac6e3350ca318359c6f6c05dbf6806d
Lucas-SousaS <lucassousa4019@gmail.com>
2024-06-26 00:25:14 -0300

    atualizando e adicionando o Carrossel de produtos

--- mich-tech/src/pages/Home.jsx
+++ mich-tech/src/pages/Home.jsx
@@ -1,9 +1,15 @@
+import CarrosselProdutos from "../components/CarrosselProdutos";
 import Header from "../components/Header";
 
 function Home() {
-    return ( 
-        <Header />
-     );
+  return (
+    <div className="flex flex-col items-center gap-7 bg-[#ebebeb] overflow-x-hidden w-screen">
+      <Header />
+      <CarrosselProdutos categoria={"iphone"} />
+      <CarrosselProdutos categoria={"notebook"}/>
+      <CarrosselProdutos categoria={"mouse"} />
+    </div>
+  );
 }
 
 export default Home;
